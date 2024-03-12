4c15088128b97302a77bebb85981faa806e87ba3
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-03-12 15:02:21 +0300

    initial commit

--- multiplayers.js
+++ multiplayers.js
@@ -119,7 +119,7 @@ async function searchOnlineUsers() {
       loadingSpinner.style.display = 'block'; // Show the loading spinner
 
       // Fetch online users from the API endpoint
-      const response = await fetch(window.env.API_URL + "/game/available", {
+      const response = await fetch(window.env.API_URL + "/user/online", {
         method: "GET",
         headers: {
           'Content-Type': 'application/json'
